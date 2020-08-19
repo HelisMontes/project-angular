c349623cf1bb5ac6ec0b0cb5bebf48901c0510bd
Helis Montes <helismontes@gmail.com>
2020-08-19 15:24:46 -0500

    Iniciando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+//Importamos la clase Component para recopilar metadatos de configuración de componentes.
+import { Component, OnInit } from '@angular/core';
 
+declare var jQuery:any;
+declare var $:any;
+
+//Decorador de la clase Componente
 @Component({
+  
+   //El selector es la etiqueta personalizada que nos permite crear Angular en el HTML y donde se va a visualizar este componente.
   selector: 'app-root',
+
+  //La plantilla url es la ruta de la vista HTML que va a imprimir este componente
   templateUrl: './app.component.html',
+
+  //La ruta del estilo es donde direccionamos la hoja de estilo exclusiva de este componente
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'project-angular';
+
+//Exportamos la clase con el nombre que nosotros declaramos en el APP MODULE
+export class AppComponent implements OnInit {
+
+	ngOnInit(){
+		/*=============================================
+    NAVEGACIÓN SCROLL
+    =============================================*/
+
+    $(".nav-link").click(function(e){
+
+      e.preventDefault();
+
+      var target = $(this).attr("href");
+      
+      $("html, body").animate({
+
+        scrollTop: $(target).offset().top
+
+      },1000, "easeOutBack")
+
+    })
+
+    /*=============================================
+    SCROLL UP
+    =============================================*/
+
+    $.scrollUp({
+      scrollText: "",
+      scrollSpeed:2000,
+      easingType: "easeOutQuint"
+    })
+    $("#scrollUp").css({
+      bottom: "20px",
+      right: "20px",
+      width: "50px",
+      height: "50px",
+      background: "url(../assets/img/flecha.jpg)"
+
+    })
+
+	}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,50 @@
+//Importa la clase BrowserModule, para buscar modulos de Angular.
 import { BrowserModule } from '@angular/platform-browser';
+
+//Importa la clase NgModule es el módulo raiz de Angular.
 import { NgModule } from '@angular/core';
 
+// AppComponent es el componente principal de Angular
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticulosComponent } from './articulos/articulos.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { GaleriaComponent } from './galeria/galeria.component';
+import { HeaderComponent } from './header/header.component';
+import { MouseComponent } from './mouse/mouse.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ArticuloComponent } from './articulo/articulo.component';
 
+//Los decoradores son funciones que modifican clases de javascript
 @NgModule({
+
+//Las declaraciones son las clases de vistas que pertenecen a ese módulo
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticulosComponent,
+    FormularioComponent,
+    GaleriaComponent,
+    HeaderComponent,
+    MouseComponent,
+    SlideshowComponent,
+    InicioComponent,
+    ArticuloComponent
   ],
+
+  //Importamos otro módulos cuya clases exportadas sean necesarias para las platillas de 
+  // de componentes declaradas en este módulo
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+
+   // Los proveedores son los creadores de servicios que este módulo contribuye a la recolección 
+   // global de servicios; Se vuelven accesibles en todas las partes de la aplicación. 
+   // Esto lo habilitamos cuando invocamos servicios desde una base de datos.
   providers: [],
   bootstrap: [AppComponent]
 })
+
+//Exportamos la clase del módulo raíz para poder ser invocada en otros archivos
 export class AppModule { }
